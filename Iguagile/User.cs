0bb25cb382e40a81bc7beb925f731a33eda03f85
minami14 <minami-git@minami-vr.sakura.ne.jp>
2019-09-29 21:26:41 +0900

    add User

---
+++ Iguagile/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iguagile
+{
+    class User
+    {
+        public int Id { get; }
+
+        public User(int id)
+        {
+            Id = id;
+        }
+    }
+}
