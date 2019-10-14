bfcec289c610be9996bf80d95c689d868726693c
Minami <minami-git@minami-vr.sakura.ne.jp>
2019-10-14 18:57:43 +0900

    add RpcMethod

---
+++ Iguagile/RpcMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Iguagile
+{
+    public class RpcMethod
+    {
+        public MethodInfo Method { get; set; }
+        public object Receiver { get; set; }
+
+        public RpcMethod(MethodInfo method, object receiver)
+        {
+            Method = method;
+            Receiver = receiver;
+        }
+    }
+}
