9293bdb22a356d62152f990c4a99c1d2001c6c79
Minami <minami-git@minami-vr.sakura.ne.jp>
2019-10-20 21:05:03 +0900

    add application name

--- Iguagile/Api/CreateRoomRequest.cs
+++ Iguagile/Api/CreateRoomRequest.cs
@@ -8,6 +8,9 @@ namespace Iguagile.Api
         [DataMember(Name = "password")]
         public string Password { get; set; }
 
+        [DataMember(Name = "application_name")]
+        public string ApplicationName { get; set; }
+
         [DataMember(Name = "version")]
         public string Version { get; set; }
 
