1b710e86caf307bcc8baf238bade2bd8ae44d418
Minami <minami-git@minami-vr.sakura.ne.jp>
2020-05-17 01:40:28 +0900

    add api test

---
+++ IguagileTests/IguagileApiTest.cs
@@ -0,0 +1,35 @@
+﻿using Iguagile.Api;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace IguagileTests
+{
+    [TestClass]
+    public class IguagileApiTest
+    {
+        private readonly string BaseUri = "http://localhost/api/v1";
+
+        [TestMethod]
+        public async Task CreateRoom()
+        {
+            using (var api = new RoomApiClient(BaseUri))
+            {
+                var req = new CreateRoomRequest
+                {
+                    ApplicationName = "appname",
+                    Version = "0.0.0",
+                    Password = "******",
+                    MaxUser = 20,
+                };
+
+                var room = await api.CreateRoomAsync(req);
+
+                Assert.AreEqual(req.ApplicationName, room.ApplicationName);
+                Assert.AreEqual(req.Version, room.Version);
+                Assert.AreEqual(req.Password, room.Password);
+                Assert.AreEqual(req.MaxUser, room.MaxUser);
+                Assert.AreEqual(!string.IsNullOrEmpty(req.Password), room.RequirePassword);
+            }
+        }
+    }
+}
