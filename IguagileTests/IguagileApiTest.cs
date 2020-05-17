ed6f682d41a03af9cc107349da089fb5426e7c7c
Minami <minami-git@minami-vr.sakura.ne.jp>
2020-05-17 15:15:43 +0900

    add SearchRoom test

--- IguagileTests/IguagileApiTest.cs
+++ IguagileTests/IguagileApiTest.cs
@@ -8,6 +8,10 @@ namespace IguagileTests
     public class IguagileApiTest
     {
         private readonly string BaseUri = "http://localhost/api/v1";
+        private readonly string ApplicationName = "ApplicationName";
+        private readonly string Version = "0.0.0";
+        private readonly string Password = "******";
+        private readonly int MaxUser = 20;
 
         [TestMethod]
         public async Task CreateRoom()
@@ -16,19 +20,43 @@ namespace IguagileTests
             {
                 var req = new CreateRoomRequest
                 {
-                    ApplicationName = "appname",
-                    Version = "0.0.0",
-                    Password = "******",
-                    MaxUser = 20,
+                    ApplicationName = ApplicationName,
+                    Version = Version,
+                    Password = Password,
+                    MaxUser = MaxUser,
                 };
 
                 var room = await api.CreateRoomAsync(req);
 
-                Assert.AreEqual(req.ApplicationName, room.ApplicationName);
-                Assert.AreEqual(req.Version, room.Version);
-                Assert.AreEqual(req.Password, room.Password);
-                Assert.AreEqual(req.MaxUser, room.MaxUser);
+                Assert.AreEqual(ApplicationName, room.ApplicationName);
+                Assert.AreEqual(Version, room.Version);
+                Assert.AreEqual(Password, room.Password);
+                Assert.AreEqual(MaxUser, room.MaxUser);
                 Assert.AreEqual(!string.IsNullOrEmpty(req.Password), room.RequirePassword);
+            }
+        }
+
+        [TestMethod]
+        public async Task SearchRoom()
+        {
+            using (var api = new RoomApiClient(BaseUri))
+            {
+                var req = new SearchRoomRequest
+                {
+                    ApplicationName = ApplicationName,
+                    Version = Version,
+                };
+
+                var rooms = await api.SearchRoomAsync(req);
+
+                Assert.AreEqual(rooms.Length, 1);
+
+                var room = rooms[0];
+
+                Assert.AreEqual(ApplicationName, room.ApplicationName);
+                Assert.AreEqual(Version, room.Version);
+                Assert.AreEqual(MaxUser, room.MaxUser);
+                Assert.AreEqual(!string.IsNullOrEmpty(Password), room.RequirePassword);
             }
         }
     }
