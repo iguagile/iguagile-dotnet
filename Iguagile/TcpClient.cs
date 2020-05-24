c18d5bddcbcb194d3716e202b175e5537b6de0a5
minami14 <minami-git@minami-vr.sakura.ne.jp>
2020-05-24 21:06:45 +0900

    delete unused using

--- Iguagile/TcpClient.cs
+++ Iguagile/TcpClient.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Iguagile.Api;
 
 namespace Iguagile
 {
--- IguagileTests/IguagileClientTests.cs
+++ IguagileTests/IguagileClientTests.cs
@@ -4,7 +4,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace IguagileTests
